5b0978ed17bbc582080e3539e197e62901c494be
Jonathan Kestler <jkest618@gmail.com>
2016-07-03 01:12:39 +0000

    finished-draft

--- jsbasics.js
+++ jsbasics.js
@@ -1,2 +1,7 @@
-alert('Hey Boulder!')
+// Use var, prompt, string literals, the + operator, and alert such that when the user opens the page, it will ask them for their name, and then respond with "Hello [YOUR_NAME]!"
+// COMPLETE
+
+var name = prompt("What is your name?");
+
+alert("Hello " + name + "!");
 
